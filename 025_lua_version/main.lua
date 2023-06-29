6ce7bfc41752f697efafa642ab6b0f290019695b
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2023-06-30 00:13:57 +0100

    lua version

---
+++ 025_lua_version/main.lua
@@ -0,0 +1,12 @@
+require('mobdebug').start()
+
+print(_VERSION)
+-- in debuger it will print: Lua 5.3
+-- in cli it will print Lua 5.4
+
+
+-- export ZBS="/Applications/ZeroBraneStudio.app/Contents/ZeroBraneStudio"
+-- export LUA_PATH="./?.lua;$ZBS/lualibs/?/?.lua;$ZBS/lualibs/?.lua"
+-- export LUA_CPATH="$ZBS/bin/linux/x86/?.so;$ZBS/bin/linux/x86/clibs/?.so"
+-- lua main.lua
+
